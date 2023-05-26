dc61556a10847fe544910caedc6dbae5f4fb1108
Animxer1 <134288691+Animxer1@users.noreply.github.com>
2023-05-27 01:19:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import getenv
 API_KEY = "DZRIHZ"
 
 if not API_KEY or API_KEY == "DZRIHZ" or API_KEY == "DZRIHZ":
-    API_KEY = getenv("DZRIHZ")
+    API_KEY = getenv("API_KEY")
 
     if not API_KEY:
         raise Exception("Please add your TechZ Api Key in config.py file")
